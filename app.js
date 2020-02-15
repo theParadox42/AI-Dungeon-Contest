6eb836d1c50656111ae3aac8041bae4c2bafe666
Paradox Programming <nathaniel.fargo@gmail.com>
2020-02-15 12:15:22 -0700

    set up app.js

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-var express = require("express"),
+var _       = require("dotenv").config(),
+    express = require("express"),
     app     = express(),
-    bodyParser = require("body-parser"),
-    ejs     = require("ejs");
+    bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+
+
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, process.env.IP, function(){
+    console.log(`App started on port ${PORT}`);
+});
