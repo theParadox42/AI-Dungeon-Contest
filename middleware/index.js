9c7c441815d768812185d78276bc5dbffe76867b
Paradox Programming <nathaniel.fargo@gmail.com>
2020-02-16 00:08:10 -0700

    fixed syntax errors
    And added a module.exports in

--- middleware/index.js
+++ middleware/index.js
@@ -6,8 +6,29 @@ middlware.loggedIn = function(req, res, next) {
     }
     req.flash("Login to do that!");
     res.redirect("/login");
-});
+};
 
 middleware.isAdmin = function(req, res, next) {
+    middleware.loggedIn(req, res, function() {
+        if (req.user.isAdmin) {
+            next();
+        } else {
+            req.flash("error", "You need to be an admin to do that!");
+            res.redirect("/");
+        }
+    });
+};
 
-});
+middleware.isJudge = function(req, res, next) {
+    middleware.loggedIn(req, res, function() {
+        if (req.user.isJudge) {
+            next();
+        } else {
+            req.flash("error", "You need to be an judge to do that!");
+            res.redirect("/");
+        }
+    });
+};
+
+
+module.exports = middleware;
