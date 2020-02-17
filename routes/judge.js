93d659c157564774e25c2f0137ff0d2976831008
Paradox Programming <nathaniel.fargo@gmail.com>
2020-02-17 15:39:58 -0700

    made some changes and improvements

--- routes/judge.js
+++ routes/judge.js
@@ -2,9 +2,23 @@ var express = require("express"),
     router = express.Router({ mergeParams: true }),
     middleware = require("../middleware"),
     Story = require("../models/story"),
-    Contest = require("../models/contest"),
-    validateStory = require("../utilities/validate-story");
+    Contest = require("../models/contest");
 
-router.get("/")
+router.get("/", middleware.isJudge, function(req, res) {
+    res.render("judge/index");
+});
+
+router.get("/contests/:tag", middleware.isJudge, middleware.contestExists, function(req, res) {
+    Contest.findById(req.contest._id).populate("stories").exec(function(err, foundContest) {
+        if (err) {
+            req.flash("error", "Error finding contest!");
+        } else if (!foundContest) {
+            req.flash("error", "No contest found!");
+        } else {
+            return res.render("judge/contests");
+        }
+        res.redirect("back");
+    });
+});
 
 module.exports = router;
