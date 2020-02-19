2e81b35b8f039320b7cecfe63e6568523dd4443c
Paradox Programming <nathaniel.fargo@gmail.com>
2020-02-18 22:06:53 -0700

    stashing

--- utilities/validate-story.js
+++ utilities/validate-story.js
@@ -5,10 +5,12 @@ function validateStory(body) {
         vs(body.description) &&
         vs(body.link) &&
         body.link.match(/https?:\/\/play.aidungeon.io\/stories\?story=.*/)) {
+        var link = body.link.replace("http:", "https:");
         return {
             title: body.title,
             description: body.description,
-            link: body.link.replace("http:", "https:")
+            link: link,
+            referenceId: link.replace("https:/"+"/play.aidungeon.io/stories?story=", "")
         }
     }
     return false;
